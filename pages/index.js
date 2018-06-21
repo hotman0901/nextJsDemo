28635dd3c2eb39adffb7c8bb3c7eac8c432cc01c
benny.hsieh <benny.hsieh@isuncloud.com>
2018-06-21 16:13:49 +0800

    add static file and add Header meta setting by next

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Header from '../components/Header/Header';
+import Head from 'next/head'
 import "../style/index.scss"
+
+
 export default class Index extends Component {
     render() {
         return (
             <div className="co_index">
+                <Head>
+                    <title>My page title</title>    
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                </Head>
                 <Header />
                 <p>hihi index</p>
                 <span className="yaya">yayayya</span>
--- pages/photo.js
+++ pages/photo.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Content from '../components/Content/content';
 import '../style/photo.scss';
 
-export const Photo = (props) => {
-    return(
+export const Photo = props => {
+    return (
         <div className="co_photo">
-            photo
+            <img src="/static/img/bg_01.jpg" alt="bg"/>
             <Content />
         </div>
-    )
-}
+    );
+};
 
 export default Photo;
