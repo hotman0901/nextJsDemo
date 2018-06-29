fd5acc47a316ab693d44ea5a3db574805525b739
benny.hsieh <benny.hsieh@isuncloud.com>
2018-06-29 11:50:43 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withSass = require('@zeit/next-sass')
+const merge = require('webpack-merge');
+
 module.exports = withSass()
+
+// 客製化build路徑
+// const withSass = require('@zeit/next-sass')
+// const merge = require('webpack-merge');
+// module.exports = merge(
+//     withSass(), 
+//     {'distDir': 'build'}
+// )
