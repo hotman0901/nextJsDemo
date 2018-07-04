5eaecdebb89114ccd1fd14782afe96d5887a1cca
benny.hsieh <hotman0901@gmail.com>
2018-07-04 15:17:51 +0800

    update

--- pages/reduxDemo.js
+++ pages/reduxDemo.js
@@ -2,39 +2,33 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { startClock, addCount, serverRenderClock } from '../actions';
 import { connect } from 'react-redux';
-import Page from '../components/ReduxSample/Page';
+import '../style/index.scss';
 
 class ReduxDemo extends React.Component {
-    // 初始化
-    static getInitialProps({ store, isServer }) {
-        store.dispatch(serverRenderClock(isServer));
-        store.dispatch(addCount());
-        return { isServer };
-    }
-
-    componentDidMount() {
-        // 啟動
-        this.timer = this.props.startClock();
-        // this.props.addCount();
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
 
     render() {
-        return <Page title="Index Page" />;
+        const { count, addCount } = this.props;
+        return (
+            <div>
+                coount: <span>{count}</span>
+                <hr />
+                <button onClick={() => addCount()}>Add To Count</button>
+            </div>
+        );
     }
 }
 
+const mapStateToProps = state => {
+    return { count: state.count };
+};
+
 const mapDispatchToProps = dispatch => {
     return {
-        addCount: bindActionCreators(addCount, dispatch),
-        startClock: bindActionCreators(startClock, dispatch)
+        addCount: bindActionCreators(addCount, dispatch)
     };
 };
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(ReduxDemo);
--- reducers/index.js
+++ reducers/index.js
@@ -1,19 +1,19 @@
 import { actionTypes } from '../actions';
 
 const initialState = {
-    lastUpdate: 0,
-    light: false,
+    // lastUpdate: 0,
+    // light: false,
     count: 0
 };
 
 // REDUCERS
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.TICK:
-            return Object.assign({}, state, {
-                lastUpdate: action.ts,
-                light: !!action.light
-            });
+        // case actionTypes.TICK:
+        //     return Object.assign({}, state, {
+        //         lastUpdate: action.ts,
+        //         light: !!action.light
+        //     });
         case actionTypes.ADD:
             return Object.assign({}, state, {
                 count: state.count + 1
